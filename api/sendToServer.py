cc97b3c51a19ad5ea4cf99474d732a09cbad6633
Emieldv <51248317+Emieldv@users.noreply.github.com>
2021-11-18 17:15:58 +0100

    Update sendToServer.py

--- api/sendToServer.py
+++ api/sendToServer.py
@@ -3,7 +3,7 @@ from settings import osc
 
 # Use this function every time your state updates -> send(statenumber)
 def sendToServer(state):
-  clientAddress = bytes("/client-{}".format(os.getenv('CLIENT_ID')), encoding='utf8')
+  clientAddress = bytes("/Client-{}".format(os.getenv('CLIENT_ID')), encoding='utf8')
   serverIp = os.getenv('SERVER_IP')
   serverPort = int(os.getenv('SERVER_PORT'))
 
